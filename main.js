9a2ca83cb6a4466dff2fa0d672d9799e91e27dcb
kingsworm0 <kingptah20@gmail.com>
2022-12-07 05:59:04 -0800

    loader file

---
+++ main.js
@@ -0,0 +1,9 @@
+const url = "https://api.financialtimes.com/analysis
+
+let text;
+
+fetch (url)
+.then(respose.json=>(text))
+.then(response.json(console.log(error))
+
+console.log("Data REsolved")
